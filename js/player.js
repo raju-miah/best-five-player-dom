77645e80138b98b29bfd0c595a8a886518156d7e
raju-miah <rajumiahpollob7@gmail.com>
2022-08-23 17:04:48 +0600

    added final total expenses

--- js/player.js
+++ js/player.js
@@ -1,4 +1,4 @@
-// console.log('hello')
+// JS Start
 
 function getTheOrderList() {
     const orderList = document.getElementById('order-list');
@@ -54,3 +54,71 @@ document.getElementById('btn-5').addEventListener('click', function () {
 document.getElementById('btn-6').addEventListener('click', function () {
     getAllPlayersList('renato', 'btn-6');
 });
+
+
+
+
+// Calculate Per Player Expenses
+
+document.getElementById('calculate-player').addEventListener('click', function () {
+    const perPlayerExpenses = document.getElementById('player-expenses-field');
+    const playerExpensesValue = parseFloat(perPlayerExpenses.value);
+
+    if (isNaN(playerExpensesValue) || playerExpensesValue < 0) {
+        alert('Please enter a vaid number')
+        return;
+    }
+
+    const playerList = document.getElementById('order-list').childElementCount;
+    const totalExpenses = playerExpensesValue * playerList;
+
+    const totalPlayerExpenses = document.getElementById('total-player-expenses');
+    totalPlayerExpenses.innerText = totalExpenses;
+    // perPlayerExpenses.value = '';
+});
+
+
+
+
+// Calcutale Manager, Coach and Final total
+
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const perPlayerExpenses = document.getElementById('player-expenses-field');
+    const playerExpensesValue = parseFloat(perPlayerExpenses.value);
+
+    if (isNaN(playerExpensesValue) || playerExpensesValue < 0) {
+        alert('Please enter a vaid number')
+        return;
+    }
+
+    const playerList = document.getElementById('order-list').childElementCount;
+    const totalExpenses = playerExpensesValue * playerList;
+
+    const totalPlayerExpenses = document.getElementById('total-player-expenses');
+    totalPlayerExpenses.innerText = totalExpenses;
+
+    const playerExpenses = parseFloat(totalPlayerExpenses.innerText);
+    const finalTotalExpenses = document.getElementById('final-total-expenses');
+
+    const managerExpenses = document.getElementById('manager-expenses');
+    const managerExpensesValue = parseFloat(managerExpenses.value);
+
+    if (isNaN(managerExpensesValue) || managerExpensesValue < 0) {
+        alert('Please enter a valid amount number');
+        return;
+    }
+
+    const coachExpenses = document.getElementById('coach-expenses');
+    const coachExpensesValue = parseFloat(coachExpenses.value);
+
+    if (isNaN(coachExpensesValue) || coachExpensesValue < 0) {
+        alert('Please enter a valid amount number');
+    }
+
+    const allFinalTotalExpenses = playerExpenses + managerExpensesValue + coachExpensesValue;
+    finalTotalExpenses.innerText = allFinalTotalExpenses;
+});
+
+
+// JS End
